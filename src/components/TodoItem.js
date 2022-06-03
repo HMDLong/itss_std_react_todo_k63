1125ab1de084c9d9c9583ac44719d903c06b2344
HMDLong <long.hmd2000@gmail.com>
2022-06-03 05:25:06 +0000

    Change Todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /* 
   【TodoItemコンポーネント】
 　・Todoアイテムを表示する
@@ -5,9 +7,20 @@
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
 function TodoItem(props) {
+  const [style, changeStyle] = useState('panel-block');
+  
+  function clickCheckbox(){
+    props.item.done = !props.item.done;
+    if(props.item.done)
+      changeStyle('panel-block has-text-grey-light');
+    else {
+      changeStyle('panel-block');
+    }
+  }
+  
   return (
-    <label className="panel-block">
-      <input type="checkbox" />
+    <label className={style}>
+      <input type="checkbox" onClick={clickCheckbox}/>
       {props.item.text}
     </label>
   );
